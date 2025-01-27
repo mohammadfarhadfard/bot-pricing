81d15fdeff7e88314f3cf5a736aa58cca7901649
MohammadFarhadfard <mohammadfarhadfard@gmail.com>
2025-01-27 18:27:05 +0330

    change orderBook

--- orderBook.js
+++ orderBook.js
@@ -1,160 +1,89 @@
 const axios = require("axios");
 require("dotenv").config();
-let WALLEX = process.env.WALLEX_Route;
-let NOBITEX = process.env.NOBITEX_Route;
-let RAMZINEX = process.env.RAMZINEX_Route;
-let OKEX = process.env.OkEXCHANGE_Route;
-let TETHERLAND = process.env.TETHERLAND_Route;
-let TABDEAL = process.env.TABDEAL_Route;
-let EXIR = process.env.EXIR_Route;
 
-//Fetch data from Wallex
-async function wallex() {
-  try {
-    const { data } = await axios.get(WALLEX);
-    const wallex_buys = data.result.bid;
-    const wallex_sells = data.result.ask;
-    return {
-      wallex_buys,
-      wallex_sells
-    };        
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
-
-// Fetch data from Ramzinex
-async function ramzinex(){
-  try {
-    const { data } = await axios.get(RAMZINEX)
-    const ramzinex_buys = data.data.buys.map((order) => ({
-      price: parseFloat(order[0]),
-      quantity: parseFloat(order[1]),
-    }));
-    const ramzinex_sells = data.data.sells.map((order) => ({
-      price: parseFloat(order[0]),
-      quantity: parseFloat(order[1]),
-    }));    
-    return {
-      ramzinex_buys,
-      ramzinex_sells,
-    };
-  } catch (error) {
-    console.error("Error fetching Ramzinex data:", error);
-  }
-}
-
-// Fetch data from Nobitex
-async function nobitex(){
-  try {
-    const { data } = await axios.get(NOBITEX);
-    const nobitex_buys = data.bids.map((order) => ({
-      price : parseFloat(order[0]),
-      quantity : parseFloat(order[1]),
-    }));
-    const nobitex_sells = data.asks.map((order) => ({
-      price : parseFloat(order[0]),
-      quantity : parseFloat(order[1])
-    }));    
-    return {
-      nobitex_buys,
-      nobitex_sells,
-    };
-   } catch (error) {
-    console.error("Error fetching Nobitex data:", error);
-  }
-}
-
-// Fetch data from okex
-async function okex() {
-  try {
-    const { data } = await axios.get(OKEX);
-    const okex_buys = data.books.bids.map((order) => ({
-      price: parseFloat(order[0]), 
-      quantity: parseFloat(order[1]), 
-    }));
-    const okex_sells = data.books.asks.map((order) => ({
-      price: parseFloat(order[0]), 
-      quantity: parseFloat(order[1]), 
-    }));
-    return {
-      okex_buys,
-      okex_sells,
-    };
-  } catch (error) {
-    console.error("Error fetching OKEx data:", error);
-  }
-}
-
-// Fetch data from tetherLand
-async function tetherLand() {
-  try {
-    const { data } = await axios.get(TETHERLAND);
-    const tetherLand_buys = data.data.markets.USDTTMN.bids.map((order) => ({
-      price: parseFloat(order.price), 
-      quantity: parseFloat(order.amount), 
-    }));
-    const tetherLand_sells = data.data.markets.USDTTMN.asks.map((order) => ({
-      price: parseFloat(order.price), 
-      quantity: parseFloat(order.amount), 
-    }));
-    return {
-      tetherLand_buys,
-      tetherLand_sells,
-    };
-  } catch (error) {
-    console.error("Error fetching TetherLand data:", error);
-  }
-}
-
-// Fetch data from tabdeal
-async function tabdeal() {
-  try {
-    const { data } = await axios.get(TABDEAL);
-    const tabdeal_buys = data.bids.map((order) => ({
-      price: parseFloat(order[0]), 
-      quantity: parseFloat(order[1]), 
-    }));
-    const tabdeal_sells = data.asks.map((order) => ({
-      price: parseFloat(order[0]), 
-      quantity: parseFloat(order[1]), 
-    }));
-    return {
-      tabdeal_buys,
-      tabdeal_sells,
-    };
-  } catch (error) {
-    console.error("Error fetching Tabdeal data:", error);
-  }
-}
-
-// Fetch data from exir
-async function exir() {
-  try {
-    const { data } = await axios.get(EXIR);
-    const exir_buys = data["usdt-irt"].bids.map((order) => ({
-      price: parseFloat(order[0]),
-      quantity: parseFloat(order[1]), 
-    }));
-    const exir_sells = data["usdt-irt"].asks.map((order) => ({
-      price: parseFloat(order[0]), 
-      quantity: parseFloat(order[1]), 
-    }));
-    return {
-      exir_buys,
-      exir_sells,
-    };
-  } catch (error) {
-    console.error("Error fetching Exir data:", error);
-  }
-}
-
-module.exports = {
-  wallex,
-  ramzinex,
-  nobitex,
-  okex,
-  tetherLand,
-  tabdeal,
-  exir,
+const exchanges = {
+  wallex: process.env.WALLEX_Route,
+  ramzinex: process.env.RAMZINEX_Route,
+  nobitex: process.env.NOBITEX_Route,
+  okex: process.env.OkEXCHANGE_Route,
+  tetherLand: process.env.TETHERLAND_Route,
+  tabdeal: process.env.TABDEAL_Route,
+  exir: process.env.EXIR_Route,
 };
+
+const logExchangeData = (exchange, data) => {
+  console.log(`[${exchange}] Data: ${JSON.stringify(data)}`);
+};
+
+const processExchangeData = async (exchange, url) => {
+  try {
+    const { data } = await axios.get(url);
+    switch (exchange) {
+      case "wallex":
+        return {
+          wallex_buys: data.result.bid,
+          wallex_sells: data.result.ask
+        };
+      case "ramzinex":
+        console.log(`Processing Ramzinex data...`);
+        return {
+          ramzinex_buys: data.data.buys.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+          ramzinex_sells: data.data.sells.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+        };
+      case "nobitex":
+      case "tabdeal":
+        console.log(`Processing ${exchange} data...`);
+        if (data.bids && data.asks) {
+          return {
+            [`${exchange}_buys`]: data.bids.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+            [`${exchange}_sells`]: data.asks.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+          };
+        } else if (data.data && data.data.bids && data.data.asks) {
+          return {
+            [`${exchange}_buys`]: data.data.bids.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+            [`${exchange}_sells`]: data.data.asks.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+          };
+        } else if (data && data.bids && data.asks) {
+          return {
+            [`${exchange}_buys`]: data.bids.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+            [`${exchange}_sells`]: data.asks.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+          };
+        } else {
+          console.error(`Error processing ${exchange} data: No books found`);
+          return {};
+        }
+      case "okex":
+        console.log(`Processing OKEx data...`);
+        return {
+          okex_buys: data.books.bids.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+          okex_sells: data.books.asks.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+        };
+      case "tetherLand":
+        return {
+          tetherLand_buys: data.data.markets.USDTTMN.bids.map((o) => ({ price: parseFloat(o.price), quantity: parseFloat(o.amount) })),
+          tetherLand_sells: data.data.markets.USDTTMN.asks.map((o) => ({ price: parseFloat(o.price), quantity: parseFloat(o.amount) })),
+        };
+      case "exir":
+        console.log(`Processing Exir data...`);
+        return {
+          exir_buys: data["usdt-irt"].bids.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+          exir_sells: data["usdt-irt"].asks.map((o) => ({ price: parseFloat(o[0]), quantity: parseFloat(o[1]) })),
+        };
+      default:
+        return {};
+    }
+  } catch (error) {
+    console.error(`Error fetching ${exchange} data:`, error);
+  }
+};
+
+const main = async () => {
+  const exchangeFunctions = Object.fromEntries(Object.keys(exchanges).map((exchange) => [exchange, () => processExchangeData(exchange, exchanges[exchange])]));
+  for (const exchange in exchangeFunctions) {
+    const data = await exchangeFunctions[exchange]();
+    console.log(`[${exchange}] Data:`);
+    console.log(data);
+  }
+};
+
+main();
